50994a7e8b90a8bde9942fa6cbead4b935e6149a
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-16 20:20:43 -0500

    Oh boy, the code doesn't finished yet

--- Challenger/number003/src/Main.java
+++ Challenger/number003/src/Main.java
@@ -12,22 +12,38 @@ public class Main {
         int resA = 0;
         int resB = 0;
 
+        int i,j=0;
+
         Integer vectorA[] = new Integer[length];
         Integer vectorB[] = new Integer[length];
+        Integer resultVector [] = new Integer[length];
 
-        for(int i=0; i<vectorA.length; i++)
+        for(i=0; i<vectorA.length; i++)
         {
             System.out.print("Write the values of vector A: ");
             vectorA[i] = object.nextInt();
         }
-        System.out.print("Values of vector A: ");
-        System.out.println(Arrays.toString(vectorA));
 
-        for(int j=0; j<vectorB.length; j++)
+        for(j=0; j<vectorB.length; j++)
         {
             System.out.print("Write the values of vector B: ");
             vectorB[j] = object.nextInt();
         }
+        System.out.println();
+        System.out.print("Values of vector A: ");
+        System.out.println(Arrays.toString(vectorA));
+        System.out.print("Values of vector B: ");
+        System.out.println(Arrays.toString(vectorB));
+
+        for(int k=0; k<vectorA.length;k++)
+        {
+            for(int l=0; l<vectorB.length;l++)
+            {
+                resultVector[l] = vectorA[i]+vectorB[j];
+            }
+        }
+        System.out.println(Arrays.toString(resultVector));
+
         System.out.print("Values of vector B: ");
         System.out.println(Arrays.toString(vectorB));
     }

8d77f4a2bdeb033b8fe0a8093c3d1384c531bc31
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-16 19:56:29 -0500

    Values of vector A has been solved

--- Challenger/number003/src/Main.java
+++ Challenger/number003/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,14 +7,19 @@ public class Main {
         Scanner object = new Scanner(System.in);
 
         System.out.println("Write the lengths of arrays: ");
-
         int length = object.nextInt();
+
         int resA = 0;
         int resB = 0;
 
         Integer vectorA[] = new Integer[length];
         Integer vectorB[] = new Integer[length];
 
-        
+        for(int i=0; i<vectorA.length; i++)
+        {
+            vectorA[i] = object.nextInt();
+        }
+        System.out.print("Values of vector A: ");
+        System.out.println(Arrays.toString(vectorA));
     }
 }

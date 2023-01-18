2d8ffb8a2e84307cf1a16c7e659cf1b77ffe1392
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-18 16:48:28 -0500

    Some changes has been saved

--- Challenger/number004/src/Main.java
+++ Challenger/number004/src/Main.java
@@ -5,6 +5,8 @@ public class Main
     public static void main(String[] args)
     {
         Scanner object = new Scanner(System.in);
+
+        System.out.print("Please write the length of array: ");
         int valueArray = object.nextInt();
 
         int i, j = 0;
@@ -12,6 +14,15 @@ public class Main
         Integer valA[] = new Integer[valueArray];
         Integer valB[] = new Integer [valA.length];
         Integer resV[] = new Integer[valA.length];
+
+        for(i=0; i< valA.length; i++)
+        {
+            System.out.print("Write the values of A: ");
+            valA[i] = object.nextInt();
+
+            System.out.print("Write the values of B: ");
+            valB[i] = object.nextInt();
+        }
 
 
     }

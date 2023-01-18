886d904384bb04bd2f5f3ff7ebd665a4594a7aed
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-18 16:39:01 -0500

    Creating the variables

--- Challenger/number004/src/Main.java
+++ Challenger/number004/src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner object = new Scanner(System.in);
+        int valueArray = object.nextInt();
+
+        int i, j = 0;
+
+        Integer valA[] = new Integer[valueArray];
+        Integer valB[] = new Integer [valA.length];
+        Integer resV[] = new Integer[valA.length];
+
+
     }
 }

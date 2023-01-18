1b2b671a36d92cfee9b2cb398658b4a4a0f3114a
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-18 17:36:07 -0500

    Problem 004 has been solved

--- Challenger/number004/src/Main.java
+++ Challenger/number004/src/Main.java
@@ -1,17 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main
 {
+    public Integer lengthArray(int amount)
+    {
+        return amount;
+    }
     public static void main(String[] args)
     {
+        Main amountVec = new Main();
         Scanner object = new Scanner(System.in);
 
         System.out.print("Please write the length of array: ");
         int valueArray = object.nextInt();
 
+        int val = amountVec.lengthArray(valueArray);
+
         int i, j = 0;
 
-        Integer valA[] = new Integer[valueArray];
+        Integer valA[] = new Integer[val];
         Integer valB[] = new Integer [valA.length];
         Integer resV[] = new Integer[valA.length];
 
@@ -22,7 +30,19 @@ public class Main
 
             System.out.print("Write the values of B: ");
             valB[i] = object.nextInt();
+
+            if(valA[i]<valB[i])
+            {
+                System.out.println("The value is minor");
+                resV[i] = valA[i];
+            }
+            else if(valA[i]>valB[i])
+            {
+                System.out.println("The value is major");
+                resV[i] = valB[i];
+            }
         }
+        System.out.print("Values compared: "+ Arrays.toString(resV));
 
 
     }

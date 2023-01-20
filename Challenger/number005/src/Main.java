c7af793fa7270497cf7846b0d67dcb24d1c2b498
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-19 20:54:52 -0500

    I have define the variables and constants

--- Challenger/number005/src/Main.java
+++ Challenger/number005/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner object = new Scanner(System.in);
+        int value = object.nextInt();
+        int i;
+
+        Integer vecA[] = new Integer[value];
+        Integer vecB[] = new Integer[vecA.length];
+        Integer vecC[] = new Integer[vecA.length];
+
+        Integer vecX[] = new Integer[vecA.length];
     }
 }

41a7f9aec581ff8d3050f784af3248ddd27b0b7a
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-19 21:23:02 -0500

    Put the for cycles and inputs from Scanner class

--- Challenger/number005/src/Main.java
+++ Challenger/number005/src/Main.java
@@ -11,5 +11,20 @@ public class Main {
         Integer vecC[] = new Integer[vecA.length];
 
         Integer vecX[] = new Integer[vecA.length];
+
+        for(i=0;i<vecA.length;i++)
+        {
+            System.out.print("Write the values of A: ");
+            vecA[i] = object.nextInt();
+
+            System.out.print("Write the values of B: ");
+            vecB[i] = object.nextInt();
+
+            System.out.print("Write the values of C: ");
+            vecC[i] = object.nextInt();
+        }
+        System.out.print("Values of A: "+Arrays.toString(vecA));
+        System.out.print("Values of B: "+Arrays.toString(vecB));
+        System.out.print("Values of C: "+Arrays.toString(vecC));
     }
 }

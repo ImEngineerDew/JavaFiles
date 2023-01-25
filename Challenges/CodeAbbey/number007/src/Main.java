201db84e9848b16d4aa2f68c9e1af4267b7adc26
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-25 17:29:59 -0500

    Creating the static arrays for fahrenheit and celsius converter

--- Challenges/CodeAbbey/number007/src/Main.java
+++ Challenges/CodeAbbey/number007/src/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args)
     {
         Scanner object = new Scanner(System.in);
+
+        System.out.print("Write the amount of array: ");
+        Integer value = object.nextInt();
+
+        Double fahrenheit[] = new Double[value];
+        Double celsius[] = new Double[fahrenheit.length];
     }
 }

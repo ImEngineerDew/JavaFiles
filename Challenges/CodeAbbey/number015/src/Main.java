90ff69d47cb35cc3af13ae29419aca27dbe5a910
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-26 15:07:38 -0500

    Creating the attribs

--- Challenges/CodeAbbey/number015/src/Main.java
+++ Challenges/CodeAbbey/number015/src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+import java.lang.Math;
 public class Main {
     public static void main(String[] args)
     {
-        
+        Scanner object = new Scanner(System.in);
     }
 }

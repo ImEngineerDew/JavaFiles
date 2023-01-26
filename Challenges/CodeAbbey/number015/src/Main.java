419711e504fda379269920d01752ba46c6bc22f5
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-01-26 15:22:47 -0500

    Generating random values

--- Challenges/CodeAbbey/number015/src/Main.java
+++ Challenges/CodeAbbey/number015/src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.lang.Math;
 public class Main {
     public static void main(String[] args)
     {
-        Scanner object = new Scanner(System.in);
+        long array[] = new long[300];
+        int i;
+
+        for(i=1; i< array.length;i++) {
+            array[i] = (int)(Math.random()*300+1);
+        }
+        System.out.println(Arrays.toString(array));
     }
 }

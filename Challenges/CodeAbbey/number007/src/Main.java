5f70a31a78f2e91a17abb23d8d0dfb7a3b0dfbe2
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-05 09:18:34 -0500

    Refactoring problem007 part 1

--- Challenges/CodeAbbey/number007/src/Main.java
+++ Challenges/CodeAbbey/number007/src/Main.java
@@ -1,15 +1,14 @@
 import java.util.*;
 import java.lang.*;
-public class Main
-{
-    public Double fahrenheitToCelsius(double fahrenheit)
-    {
-        double celsius = (fahrenheit - 32)/1.8;
+
+public class Main {
+    public Double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (fahrenheit - 32) / 1.8;
 
         return celsius;
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         int i;
         Scanner object = new Scanner(System.in);
         Main conversion = new Main();
@@ -17,25 +16,21 @@ public class Main
         System.out.print("Write the amount of array: ");
         Integer value = object.nextInt();
 
-        Double fahrenheit[] = new Double[value];
-        Double celsius[] = new Double[fahrenheit.length];
-        Long celsiusRounded [] = new Long[fahrenheit.length];
+        Double[] fahrenheit = new Double[value];
+        Double[] celsius = new Double[fahrenheit.length];
+        Long[] celsiusRounded = new Long[fahrenheit.length];
 
-        StringBuffer rndC =  new StringBuffer();
+        StringBuffer rndC = new StringBuffer();
 
-        for(i=0; i< fahrenheit.length;i++)
-        {
+        for (i = 0; i < fahrenheit.length; i++) {
             fahrenheit[i] = object.nextDouble();
             celsius[i] = conversion.fahrenheitToCelsius(fahrenheit[i]);
-            celsiusRounded[i] =  Math.round(celsius[i]);
+            celsiusRounded[i] = Math.round(celsius[i]);
         }
-
-        for(int j=0; j<celsiusRounded.length;j++)
-        {
+        for (int j = 0; j < celsiusRounded.length; j++) {
             rndC.append(celsiusRounded[j]).append(" ");
         }
-
-        System.out.println("Fahrenheit degrees :"+Arrays.toString(fahrenheit));
-        System.out.print("Celsius degrees: "+rndC);
+        System.out.println("Fahrenheit degrees :" + Arrays.toString(fahrenheit));
+        System.out.print("Celsius degrees: " + rndC);
     }
 }

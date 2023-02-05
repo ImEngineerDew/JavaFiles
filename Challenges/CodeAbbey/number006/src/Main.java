7cd38cbf119cd2e45fa3ab53bd6ea30d866b8f7b
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-05 09:38:37 -0500

    Refactoring problem 006 has been ended

--- Challenges/CodeAbbey/number006/src/Main.java
+++ Challenges/CodeAbbey/number006/src/Main.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 import java.util.*;
-public class Main
-{
-    public Double divisorOperation(int a, int b)
-    {
-        return (double)a/b;
+
+public class Main {
+    public Double divisorOperation(int a, int b) {
+        return (double) a / b;
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
         int i;
         Main division = new Main();
@@ -15,32 +14,36 @@ public class Main
         System.out.print("Write the amount of arrays: ");
         int valueArr = object.nextInt();
 
-        Integer vectorA[] = new Integer[valueArr];
-        Integer vectorB[] = new Integer[vectorA.length];
-        Long rounded[] = new Long[vectorA.length];
-        Double vectorDiv[] = new Double[vectorA.length];
+        Integer[] vectorA = new Integer[valueArr];
+        Integer[] vectorB = new Integer[vectorA.length];
+        Long[] rounded = new Long[vectorA.length];
+        Double[] vectorDiv = new Double[vectorA.length];
 
-        for(i=0;i<vectorA.length;i++)
-        {
+        System.out.println(" ");
+        for (i = 0; i < vectorA.length; i++) {
             System.out.print("Write the values of vector A: ");
             vectorA[i] = object.nextInt();
-
+        }
+        System.out.println(" ");
+        for (i = 0; i < vectorB.length; i++) {
             System.out.print("Write the values of vector B: ");
             vectorB[i] = object.nextInt();
 
-            vectorDiv[i]= division.divisorOperation(vectorA[i],vectorB[i]);
+            vectorDiv[i] = division.divisorOperation(vectorA[i], vectorB[i]);
 
             rounded[i] = Math.round(vectorDiv[i]);
         }
 
-        StringBuffer rnd =  new StringBuffer();
+        StringBuffer rnd = new StringBuffer();
 
-        for(int j=0;j< rounded.length;j++)
-        {
+        for (int j = 0; j < rounded.length; j++) {
             rnd.append(rounded[j]).append(" ");
         }
-
-        System.out.println("Rounded: "+rnd);
-        System.out.println("Not rounded: "+Arrays.toString(vectorDiv));
+        for(int k = 0; k<vectorDiv.length;k++){
+            rnd.append(vectorDiv[k]).append(" ");
+        }
+        System.out.println(" ");
+        System.out.println("Rounded: " + rnd);
+        System.out.println("Not rounded: " + Arrays.toString(vectorDiv));
     }
 }

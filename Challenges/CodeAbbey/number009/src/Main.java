be886558522c687e9268463a89aafea526570def
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-06 20:59:38 -0500

    Solving some issues from problem009

--- Challenges/CodeAbbey/number009/src/Main.java
+++ Challenges/CodeAbbey/number009/src/Main.java
@@ -5,45 +5,32 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner obj = new Scanner(System.in);
-        //System.out.print("Write the size of array: ");
-        //Integer size = obj.nextInt();
         int i;
 
-        Integer triangle[] = new Integer[3];
-        //Integer sideC[] = new Integer[triangle.length];
+        Integer[] triangle = new Integer[3];
 
-        for(i=0; i<triangle.length;i++) {
+        for (i = 0; i < triangle.length; i++) {
             System.out.print("Write the sides of triangle: ");
             triangle[i] = obj.nextInt();
             //sideC[i] = triangle[0]+triangle[1];
         }
         System.out.println(" ");
-        System.out.println("Side A: "+triangle[0]);
-        System.out.println("Side B: "+triangle[1]);
-        System.out.println("Side C: "+triangle[2]);
+        System.out.println("Side A: " + triangle[0]);
+        System.out.println("Side B: " + triangle[1]);
+        System.out.println("Side C: " + triangle[2]);
 
-        Integer condOne = triangle[0]+triangle[1];
-        Integer condTwo = triangle[0]+triangle[2];
-        Integer condThree = triangle[1] +triangle[2];
+        Integer condOne = triangle[0] + triangle[1];
+        Integer condTwo = triangle[0] + triangle[2];
+        Integer condThree = triangle[1] + triangle[2];
 
-        if((condOne)>triangle[2]) {
+        if ((condOne) > triangle[2]) {
             System.out.println("It does pass the condition a+b>c");
         }
-        else {
-            System.out.println("It doesn't pass the condition a+b>c");
-        }
-        if((condTwo)>triangle[1]) {
-            System.out.println("It does pass the condition a+c>b");
-        }
-        else {
-            System.out.println("It doesn't pass the condition a+c>b");
-        }
-        if((condThree)>triangle[0])
-        {
-            System.out.println("It does pass the condition b+c>a");
-        }
-        else {
-            System.out.println("It doesn't pass the condition b+c>a");
+            if ((condTwo) > triangle[1]) {
+                System.out.println("It does pass the condition a+c>b");
+            }
+                if ((condThree) > triangle[0]) {
+                    System.out.println("It does pass the condition b+c>a");
+                }
         }
     }
-}

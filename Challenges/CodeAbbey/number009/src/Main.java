43b6db8b8c99f3664f3cdbea3a7fcbc16d2b5611
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-08 19:57:32 -0500

    Fixed the problem009 again

--- Challenges/CodeAbbey/number009/src/Main.java
+++ Challenges/CodeAbbey/number009/src/Main.java
@@ -1,44 +1,40 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
-    public Integer resultSum(int a, int b) {
-        return a + b;
-    }
-
     public static void main(String[] args) {
 
-        Main tri = new Main();
         Scanner obj = new Scanner(System.in);
 
         System.out.print("Write the size of array: ");
         int size = obj.nextInt();
-        int answer = 0;
         int i;
 
-        Integer[] sideA = new Integer[size];
-        Integer[] sideB = new Integer[sideA.length];
-        Integer[] sideC = new Integer[sideA.length];
+        Long[] sideA = new Long[size];
+        Long[] sideB = new Long[sideA.length];
+        Long[] sideC = new Long[sideA.length];
+
+        Long[] sum = new Long[sideA.length];
 
         for (i = 0; i < sideA.length; i++) {
             System.out.print("Write the side A of triangle: ");
-            sideA[i] = obj.nextInt();
+            sideA[i] = obj.nextLong();
         }
         for (i = 0; i < sideB.length; i++) {
             System.out.print("Write the side B of triangle: ");
-            sideB[i] = obj.nextInt();
+            sideB[i] = obj.nextLong();
         }
         for (i = 0; i < sideC.length; i++) {
             System.out.print("Write the side C of triangle: ");
-            sideC[i] = obj.nextInt();
+            sideC[i] = obj.nextLong();
         }
         /**Triangle # 1**/
-        for (int k = 0; k < sideC.length; k++) {
-            if (tri.resultSum(sideA[k], sideB[k]) > sideC[k]) {
-                if (tri.resultSum(sideA[k], sideC[k]) > sideB[k]) {
-                    if (tri.resultSum(sideB[k], sideC[k]) > sideA[k]) {
-                        System.out.print(answer + 1+" ");
+        int answer = 0;
+
+        for (int k = 0; k < sum.length; k++) {
+            if (sideA[k]+sideB[k] > sideC[k]) {
+                if (sideA[k]+sideC[k]>sideB[k]) {
+                    if (sideB[k]+sideC[k]> sideA[k]) {
+                        System.out.print(answer+1);
                     }
                 }
             } else {

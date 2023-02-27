90fecc19260badb87272f381b48491ed321e6649
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-02-27 18:23:47 -0500

    Dice rolling almost been sucedded

--- Challenges/CodeAbbey/number43/src/Main.java
+++ Challenges/CodeAbbey/number43/src/Main.java
@@ -1,12 +1,23 @@
 import java.lang.Math;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double randomGen = Math.random();
-        int diceNumber = (int)Math.round(randomGen*6);
 
+        //Scanner object = new Scanner(System.in);
+        int sizeArray = 6;
+        int diceNumber = 0;
+        double randomGen = 0.0;
+        Integer arrayDice [] = new Integer[sizeArray];
 
+        for(int i =0; i<sizeArray; i++)
+        {
+            randomGen = Math.random();
+            diceNumber = (int)Math.round(randomGen*sizeArray);
+
+            arrayDice[i] = diceNumber;
+        }
         System.out.println("Random generated: "+randomGen);
-        System.out.println("Pseudodice generated: "+diceNumber);
+        System.out.println("Pseudodice generated: "+Arrays.toString(arrayDice));
     }
 }

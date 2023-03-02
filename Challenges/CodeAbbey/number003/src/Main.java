cc690793867024c152980ed488619d124e17ca70
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-02 11:31:20 -0500

    Putting comments

--- Challenges/CodeAbbey/number003/src/Main.java
+++ Challenges/CodeAbbey/number003/src/Main.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-       
+
         Scanner object = new Scanner(System.in);
         int pos = 0;
 
         System.out.println("Write the size of vector: ");
         Integer size = object.nextInt();
 
-        Integer vectorC[] = new Integer[size];
-        readArray(vectorC,pos,object);
-        showArray(vectorC,pos);
+        /** Define the array and its size **/
+        Integer vectorA[] = new Integer[size];
+        readArray(vectorA,pos,object);
+
+        /** Show the arrays prior to sum **/
+        showArray(vectorA,pos);
     }
     public static void showArray(Integer vector[], Integer i) {
         if(i.equals(vector.length-1)) {
@@ -28,7 +31,7 @@ public class Main {
         if(i.equals(vector.length)) {
             return;
         }
-            System.out.println("Write the numbers of the array: ");
+            System.out.print("Write the numbers of the array: ");
             vector[i] = object.nextInt();
             readArray(vector,i+1,object);
     }

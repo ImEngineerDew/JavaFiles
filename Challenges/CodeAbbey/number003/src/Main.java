a37e64e9a158bd7dde05ea2a40b266277af5c5bc
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-02 14:12:38 -0500

    Keeping the last changes, before to compare codes

--- Challenges/CodeAbbey/number003/src/Main.java
+++ Challenges/CodeAbbey/number003/src/Main.java
@@ -8,48 +8,35 @@ public class Main {
         int pos = 0;
 
         System.out.println("Write the size of vector: ");
-        Integer size = object.nextInt();
+        int size = object.nextInt();
 
         /** Define the array and its size **/
         Integer vectorA[] = new Integer[size];
-        Integer vectorB[] = new Integer[vectorA.length];
-        readArray(vectorA,pos,object);
-        readArray(vectorB,pos,object);
+        Integer vectorB[] = new Integer[size];
+        readArray(vectorA, pos, object);
+        readArray(vectorB, pos, object);
 
         /** Show the arrays prior to sum **/
-        showArray(vectorA,pos);
-        showArray(vectorB,pos);
-
-        sumArray(vectorA,vectorB,pos);
+        showArray(vectorA, pos);
+        showArray(vectorB, pos);
     }
     public static void showArray(Integer vector[], Integer i) {
-        if(i.equals(vector.length-1)) {
+        if (i.equals(vector.length - 1)) {
+            //System.out.println("Position " + i + ": " + vector[i]);
             System.out.print(vector[i]+" ");
-        }
-        else{
+            System.out.println(" ");
+        } else {
+            //System.out.println("Position " + i + ": " + vector[i]);
             System.out.print(vector[i]+" ");
-            showArray(vector,i+1);
+            showArray(vector, i + 1);
         }
     }
-    public static void readArray(Integer vector[],Integer i, Scanner object)
-    {
-        if(i.equals(vector.length)) {
+    public static void readArray(Integer vector[], Integer i, Scanner object) {
+        if (i.equals(vector.length)) {
             return;
         }
-            System.out.print("Write the numbers on the position "+i+": ");
-            vector[i] = object.nextInt();
-            readArray(vector,i+1,object);
-    }
-    public static int [] sumArray(Integer vectorA[],Integer vectorB[],Integer i)
-    {
-        if(i.equals(vectorA.length) || i.equals(vectorB.length))
-        {
-             return 0;
-        }
-        else {
-            int res = vectorA[i]+vectorB[i];
-            System.out.println(res);
-            return res +sumArray(vectorA,vectorB,i+1);
-        }
+        System.out.print("Write the numbers on the position " + i + ": ");
+        vector[i] = object.nextInt();
+        readArray(vector, i + 1, object);
     }
 }

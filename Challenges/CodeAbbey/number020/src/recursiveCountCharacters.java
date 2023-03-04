83b2cbc92b64218d9108475f16f9ee382420cdf0
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-04 17:44:36 -0500

    Putting the recursive programming for the problem 020

--- Challenges/CodeAbbey/number020/src/recursiveCountCharacters.java
+++ Challenges/CodeAbbey/number020/src/recursiveCountCharacters.java
@@ -1,26 +1,39 @@
 import java.util.Scanner;
 
 public class recursiveCountCharacters {
+
+  static char aWord = 'a';
+  static char eWord = 'e';
+  static char iWord = 'i';
+  static char oWord = 'o';
+  static char uWord = 'u';
+
   public static void main(String[] args) {
 
 
-  }
+    Scanner objeto = new Scanner(System.in);
+    int index = 0;
 
-  public static void showVector(Integer vector[], Integer index) {
-    if (index.equals(vector.length)) {
-      System.out.println(vector[index]);
-    } else {
-      showVector(vector, index + 1);
+    System.out.println("Write the size of array: ");
+    Integer size = objeto.nextInt();
+
+    String frase = "";
+
+    objeto.nextLine();
+
+    String vectorString[] = new String[size];
+    Integer charCounter[] = new Integer[vectorString.length];
+
+    readVectorString(vectorString, index, objeto);
+  }
+  public static void readVectorString(String vectorX[], Integer i, Scanner object) {
+    if (i == vectorX.length) {
+      return;
     }
-  }
+    System.out.println("Write your quote: ");
+    vectorX[i] = object.nextLine();
 
-  public static void readVectorString(String vector[], Scanner object, Integer index)
-  {
-      if(index.equals(vector.length))
-      {
-        return;
-      }
-      vector[index] = object.nextLine();
-      readVectorString(vector,object,index+1);
+    System.out.println(vectorX[i]);
+    readVectorString(vectorX, i + 1, object);
   }
 }

788882ce7050f7c47b9a618f347d28b6789bfaf9
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-04 19:15:19 -0500

    Problem 020 has been solved

--- Challenges/CodeAbbey/number020/src/recursiveCountCharacters.java
+++ Challenges/CodeAbbey/number020/src/recursiveCountCharacters.java
@@ -6,7 +6,7 @@ public class recursiveCountCharacters {
     Scanner objeto = new Scanner(System.in);
     int index = 0;
 
-    System.out.println("Write the size of array: ");
+    System.out.print("Write the size of array: ");
     Integer size = objeto.nextInt();
 
     objeto.nextLine();
@@ -14,27 +14,58 @@ public class recursiveCountCharacters {
     String vectorString[] = new String[size];
     Integer charCounter[] = new Integer[vectorString.length];
 
-    readVectorString(vectorString, index, objeto);
-
-    showVectorString(vectorString,index);
+    readVectorString(vectorString, charCounter,index,objeto);
+    countCharacters(charCounter,index);
   }
 
-  public static void readVectorString(String vectorX[], Integer index, Scanner object) {
-    if (index== vectorX.length) {
+  public static void readVectorString(String vectorX[],Integer charCounter[], Integer index, Scanner object) {
+    if (index == vectorX.length) {
       return;
     }
+    Integer counter = 0;
+    char aWord = 'a';
+    char eWord = 'e';
+    char iWord = 'i';
+    char oWord = 'o';
+    char uWord = 'u';
+
     String quote = object.nextLine();
     vectorX[index] = quote;
 
-    readVectorString(vectorX,  index + 1, object);
+    for (int k = 0; k < quote.length(); k++) {
+      if (quote.charAt(k) == aWord) {
+        counter++;
+      } else if (quote.charAt(k) == eWord) {
+        counter++;
+      } else if (quote.charAt(k) == iWord) {
+        counter++;
+      } else if (quote.charAt(k) == oWord) {
+        counter++;
+      } else if (quote.charAt(k) == uWord) {
+        counter++;
+      }
+      charCounter[index] = counter;
+    }
+    readVectorString(vectorX, charCounter,index + 1, object);
   }
 
   public static void showVectorString(String vectorX[], Integer index) {
-    if (index==vectorX.length - 1) {
-      System.out.println(vectorX[index]);
+    if (index == vectorX.length - 1) {
+      System.out.print(vectorX[index] + " ");
     } else {
-      System.out.println(vectorX[index]);
+      System.out.print(vectorX[index] + " ");
       showVectorString(vectorX, index + 1);
+    }
+  }
+
+  public static void countCharacters(Integer vector[], Integer index)
+  {
+    if(index==vector.length -1)
+    {
+      System.out.print(vector[index]+" ");
+    }else {
+      System.out.print(vector[index]+" ");
+      countCharacters(vector,index+1);
     }
   }
 }

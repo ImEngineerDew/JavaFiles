d33c8c8ccb3279492adc383c78e66ea44551ea80
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-05 22:30:10 -0500

    Developing the sideB of your triangle

--- Challenges/CodeAbbey/number009/src/TrianglesRecursive.java
+++ Challenges/CodeAbbey/number009/src/TrianglesRecursive.java
@@ -4,22 +4,34 @@ public class TrianglesRecursive {
   public static void main(String args[]) {
 
     Scanner element = new Scanner(System.in);
+
+    System.out.println("Write the size of your array: ");
     Integer size = element.nextInt();
     int i = 0;
 
     Long sideA[] = new Long[size];
     Long sideB[] = new Long[sideA.length];
     Long sideC[] = new Long[sideA.length];
+
+    readSideA(sideA, element, i);
     
-    readSideA(sideA,element,i);
   }
 
   public static void readSideA(Long sideA[], Scanner object, Integer index) {
     if (index.equals(sideA.length)) {
       return;
     }
-      sideA[index] = object.nextLong();
-      System.out.println(sideA[index]);
-      readSideA(sideA, object, index + 1);
+    sideA[index] = object.nextLong();
+    System.out.println(sideA[index]);
+    readSideA(sideA, object, index + 1);
+  }
+
+  public static void readSideB(Long sideB[], Scanner object, Integer index) {
+    if (index.equals(sideB.length)) {
+      return;
     }
+    sideB[index] = object.nextLong();
+    System.out.println(sideB[index]);
+    readSideB(sideB, object, index + 1);
+  }
 }

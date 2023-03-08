ffc0fd07b0b075c10c09357b93e6220e3d34e4d5
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-08 15:40:20 -0500

    Adding the Scanner class

--- RandomProjects/BubbleSort/src/exampleTwoDim.java
+++ RandomProjects/BubbleSort/src/exampleTwoDim.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+
 public class exampleTwoDim {
   public static void main(String args[]) {
-    int vectorList[] = {1, 2, 3, 4, 5};
-    printVector(vectorList);
+
+    int i = 0,j=0;
+    Scanner obj = new Scanner(System.in);
+
+    System.out.print("Write the size of your array: ");
+    int size = obj.nextInt();
+
+    int vectorList[] = new int[size];
+    readArray(vectorList,i,obj);
+    bubbleRecursive(vectorList,i,j);
   }
 
-  public static void printVector(int vector[]) {
-    bubbleRecursive(vector, 0, 0);
+  public static void readArray(int vecRnd[], int i, Scanner sc)
+  {
+    System.out.print("Write the numbers: ");
+    vecRnd[i] = sc.nextInt();
+    readArray(vecRnd,i,sc);
   }
 
   public static void bubbleRecursive(int vecRnd[], int i, int j) {
     if (i == vecRnd.length) {
       return;
     } else if (j == vecRnd.length) {
-
+      //Put here the bubbleSort
       bubbleRecursive(vecRnd, i + 1, i + 1);
     } else {
       if (j == i) {
@@ -21,5 +34,4 @@ public class exampleTwoDim {
       bubbleRecursive(vecRnd, i, j + 1);
     }
   }
-
 }

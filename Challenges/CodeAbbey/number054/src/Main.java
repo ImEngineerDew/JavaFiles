1783fca7670b56b42731e9b55793c696513d3b7e
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-16 16:54:45 -0500

    Ending the noRecursive code

--- Challenges/CodeAbbey/number054/src/Main.java
+++ Challenges/CodeAbbey/number054/src/Main.java
@@ -6,22 +6,32 @@ public class Main {
 
     Scanner object = new Scanner(System.in);
 
-    System.out.println("Write the size of your array: ");
+    System.out.print("Write the size of your array: ");
     Integer size = object.nextInt();
 
     Integer vector[] = new Integer[size];
 
     Integer s = 0;
+    Integer a = 0, b = 0, c = 0;
 
-    for(int i=0;i<vector.length;i++)
-    {
-      System.out.println("Write the number to calulate their squares: ");
+    for (int i = 0; i < vector.length; i++) {
+      System.out.print("Write the number to calulate their squares: ");
       s = object.nextInt();
       vector[i] = s;
-
-      
     }
 
+    for (a = 1; a < s; a++) {
+      for (b = a + 1; b < s; b++) {
+        c= (int)Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        if(a+b+c==s)
+        {
+          if(c>a && c>b && Math.pow(a,2)+Math.pow(b,2) == Math.pow(c,2))
+          {
+            System.out.println(Math.pow(c,2));
+          }
+        }
+      }
+    }
     System.out.println(Arrays.toString(vector));
   }
 

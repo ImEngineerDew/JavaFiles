4a6dddb79a4e5fbcdd36b511f614fa445ccdf1a5
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-16 21:38:23 -0500

    Completed the first recursive method

--- Challenges/CodeAbbey/number054/src/recursivePythagorean.java
+++ Challenges/CodeAbbey/number054/src/recursivePythagorean.java
@@ -1,10 +1,24 @@
 import java.util.Scanner;
+
 public class recursivePythagorean {
   public static void main(String args[]) {
 
   }
 
   public static void readVector(Integer vec[], Scanner sc, Integer index) {
+    if(!index.equals(vec.length)){
+      return;
+    }
+    Integer s = sc.nextInt();
+    Integer cSquared = 0;
+
+    calculateSquaresAux(vec, s, 1, 2, cSquared);
+
+    vec[index] = cSquared;
+    readVector(vec,sc,index+1);
+  }
+  public static void calculateSquaresAux(Integer vector[], int s, int a, int B, int cSquared)
+  {
 
   }
 }

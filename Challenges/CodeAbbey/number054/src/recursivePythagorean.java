895a2924d4376a5f3541c6cac3b8a535016e74d8
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-16 21:59:37 -0500

    Almost ready

--- Challenges/CodeAbbey/number054/src/recursivePythagorean.java
+++ Challenges/CodeAbbey/number054/src/recursivePythagorean.java
@@ -4,31 +4,40 @@ public class recursivePythagorean {
   public static void main(String args[]) {
 
     Scanner object = new Scanner(System.in);
-    System.out.println("Write the size of your array: ");
-    
+    System.out.print("Write the size of your array: ");
+    Integer size = object.nextInt();
+
+    Integer pythagoras[] = new Integer[size];
+
+    readVector(pythagoras, object, 0);
+
+    for(int i=0; i<pythagoras.length;i++)
+    {
+      System.out.println(pythagoras[i]);
+    }
   }
 
   public static void readVector(Integer vec[], Scanner sc, Integer index) {
     if (!index.equals(vec.length)) {
-      return;
+
+      System.out.print("Write the following numbers: ");
+      Integer s = sc.nextInt();
+      Integer cSquared = calculateSquaresAux(s,1,2);
+
+     vec[index] = cSquared;
+      readVector(vec, sc, index + 1);
     }
-    Integer s = sc.nextInt();
-    Integer cSquared = 0;
-
-    calculateSquaresAux(vec, s, 1, 2, cSquared);
-
-    vec[index] = cSquared;
-    readVector(vec, sc, index + 1);
   }
 
-  public static void calculateSquaresAux(Integer vector[], int s, int a, int b, int cSquared) {
+  public static int calculateSquaresAux(int s, int a, int b) {
     if (a >= s || b >= s) {
-      return;
+      return 0;
     }
+    int cSquared = 0;
     int c = (int) Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
     if (a + b + c == s && c > a && c > b && Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) {
       cSquared = (int) Math.pow(c, 2);
     }
-    calculateSquaresAux(vector,s,a,b,cSquared);
+    return cSquared+calculateSquaresAux(s, a + 1, b + 1);
   }
 }

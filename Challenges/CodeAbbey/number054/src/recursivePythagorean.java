b415b7be8f1845b74a1a730d79689e7925e6c750
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-16 21:34:39 -0500

    Making recursive pythagorean solution

---
+++ Challenges/CodeAbbey/number054/src/recursivePythagorean.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class recursivePythagorean {
+  public static void main(String args[]) {
+
+  }
+
+  public static void readVector(Integer vec[], Scanner sc, Integer index) {
+
+  }
+}

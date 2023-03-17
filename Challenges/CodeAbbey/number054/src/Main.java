f0a84ea02beb3fe264b6682516146fc72998ff49
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-17 12:38:53 -0500

    Applying the sugestion of Euclidean theorem instead of brute force to solve the Pythagorean triplets

--- Challenges/CodeAbbey/number054/src/Main.java
+++ Challenges/CodeAbbey/number054/src/Main.java
@@ -2,7 +2,25 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int s = input.nextInt();
 
+    for (int m = 2; m <= Math.sqrt(s); m++) {
+      for (int n = 1; n < m; n++) {
+        int a = (int)Math.pow(m,2) - n * n;
+        int b = 2 * m * n;
+        int c = m * m + n * n;
+
+        if (a + b + c == s) {
+          System.out.println(a + " " + b + " " + c);
+          System.out.println("Value of c^2: "+(int)Math.pow(c,2));
+        }
+      }
+    }
+  }
+  }
+
+    /**
     Scanner object = new Scanner(System.in);
     int s = object.nextInt();
 
@@ -15,4 +33,4 @@ public class Main {
       }
     }
   }
-}
+     **/

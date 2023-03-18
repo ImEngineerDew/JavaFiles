1ea4f839fbc54b96308d1a7caffe2fe9d5e187bc
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-18 16:46:47 -0500

    Changing from long to double

--- Challenges/CodeAbbey/number054/src/recursivePythagorean.java
+++ Challenges/CodeAbbey/number054/src/recursivePythagorean.java
@@ -8,35 +8,43 @@ public class recursivePythagorean {
     System.out.print("Write the size of your array: ");
     int size = object.nextInt();
 
-    Long pythagoras[] = new Long[size];
-    readVector(pythagoras,object,0);
-    System.out.println(Arrays.toString(pythagoras));
+    Double pythagoras[] = new Double[size];
+    readVector(pythagoras, object, 0);
+    showResult(pythagoras,0);
   }
-  public static void readVector(Long vec[],Scanner sc, Integer index){
+
+  public static void readVector(Double vec[], Scanner sc, Integer index) {
+    if (!index.equals(vec.length)) {
+      long s = sc.nextLong();
+      vec[index] = calculateSquaresAux(s, 1, 2);
+      readVector(vec, sc, index + 1);
+    }
+  }
+  public static void showResult(Double vec[], Integer index)
+  {
     if(!index.equals(vec.length))
     {
-      long s = sc.nextLong();
-      vec[index] = calculateSquaresAux(s,1,2);
-      readVector(vec,sc,index+1);
+      System.out.println(vec[index]+" ");
+      showResult(vec,index+1);
     }
   }
-  
-  public static long calculateSquaresAux(long s, long m, long n) {
-    if (m>=Math.sqrt(s)) {
-      return 0;
-    }
-    if (n>=m) {
-      return calculateSquaresAux(s,m+1,1);
-    }
-    long a = (int)Math.pow(m,2) - n * n;
-    long b = 2 * m * n;
-    long c =((int)Math.pow(m,2)) + ((int)Math.pow(n,2));
 
-    if (a + b + c == s) {
-      //System.out.println(a + " " + b + " " + c);
-      long cSquared = (int)Math.pow(c,2);
-      return cSquared;
+  public static double calculateSquaresAux(long s, long m, long n) {
+    while (m < Math.sqrt(s)) {
+      while (n < m) {
+        double a = (Math.pow(m, 2) - n * n);
+        double b = 2 * m * n;
+        double c = (Math.pow(m, 2)) + (Math.pow(n, 2));
+
+        if (a + b + c == s) {
+          double cSquared = Math.pow(c, 2);
+          return cSquared;
+        }
+        n++;
+      }
+      m++;
+      n = 1;
     }
-    return calculateSquaresAux(s,m,n+1);
+    return 0;
   }
 }

9664637cba77f01b3797dd433395fd49adf86ca4
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-18 17:21:12 -0500

    Format the output that can show the results

--- Challenges/CodeAbbey/number054/src/recursivePythagorean.java
+++ Challenges/CodeAbbey/number054/src/recursivePythagorean.java
@@ -21,14 +21,15 @@ public class recursivePythagorean {
 
   public static void showResult(Double vec[], Integer index) {
     if (!index.equals(vec.length)) {
-      System.out.printf("%.0f",vec[index]);
+      System.out.printf("%.0f", vec[index]);
+      System.out.println(" ");
       showResult(vec, index + 1);
     }
   }
 
   public static Double calculateSquaresAux(long s, long m, long n) {
-    while (m < Math.sqrt(s)) {
-      while (n < m) {
+    for (m = 2; m <= Math.sqrt(s); m++) {
+      for (n = 1; n < m; n++) {
         Double a = Math.pow(m, 2) - n * n;
         Double b = 2.0 * m * n;
         Double c = (Math.pow(m, 2)) + (Math.pow(n, 2));
@@ -37,10 +38,7 @@ public class recursivePythagorean {
           Double cSquared = Math.pow(c, 2);
           return cSquared;
         }
-        n++;
       }
-      m++;
-      n = 1;
     }
     return 0.0;
   }

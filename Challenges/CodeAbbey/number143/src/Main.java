a42a6315f914ebdc7746b458d6bf0c7425197bb6
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-22 21:16:38 -0500

    Write the code lines for calculate the GCD

--- Challenges/CodeAbbey/number143/src/Main.java
+++ Challenges/CodeAbbey/number143/src/Main.java
@@ -6,5 +6,16 @@ public class Main {
 
     System.out.println("Write the first number: ");
     Integer firstNumber = object.nextInt();
+
+    System.out.println("Write the second number: ");
+    Integer secondNumber = object.nextInt();
+
+    System.out.println("Write the third number: ");
+    Integer thirdNumber = object.nextInt();
+  }
+
+  public static Integer commonGreaterNumber(Integer a, Integer b)
+  {
+
   }
 }

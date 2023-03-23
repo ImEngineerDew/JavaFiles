16e324963e1773ffdb669ed995834caad03aebd2
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-23 16:00:23 -0500

    Create the recursive method for the problem 143

---
+++ Challenges/CodeAbbey/number143/src/recursiveGCD.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class recursiveGCD
+{
+    public static void main(String args[])
+    {
+       Scanner object = new Scanner(System.in);
+       Integer size = object.nextInt();
+
+       Integer vector[] = new Integer[size];
+       Integer valA[] = new Integer[vector.length];
+       Integer valB[] = new Integer[vector.length];
+    }
+}

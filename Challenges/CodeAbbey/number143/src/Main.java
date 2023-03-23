049ccad70e0774cd85398cce5d68a825ae729400
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-22 21:13:32 -0500

    Problem 154 is starting now

---
+++ Challenges/CodeAbbey/number143/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner object = new Scanner(System.in);
+
+    System.out.println("Write the first number: ");
+    Integer firstNumber = object.nextInt();
+  }
+}

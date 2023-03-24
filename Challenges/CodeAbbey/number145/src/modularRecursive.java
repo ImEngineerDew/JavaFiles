70622c7455f41235ac9ff8f5697e4c05a793466f
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-24 15:28:29 -0500

    Design a modular vectoria calculator

--- Challenges/CodeAbbey/number145/src/modularRecursive.java
+++ Challenges/CodeAbbey/number145/src/modularRecursive.java
@@ -2,20 +2,36 @@ import java.util.Scanner;
 public class modularRecursive {
   public static void main(String args[])
   {
-    //Long res = calculateModular(14L,28L,219431273L);
-    System.out.println(res);
-  }
+    Scanner object = new Scanner(System.in);
+    Integer size = object.nextInt();
+
+    Long base[] = new Long[size];
+    Long exp[] = new Long[base.length];
+    Long module[] = new Long[base.length];
+
+    readVector(base,exp,module,object,0);
+   }
 
   public static void readVector(Integer vecA[],Integer vecB[],Integer vecC[],Scanner sc, Integer index)
   {
     if(!index.equals(vecA.length))
     {
-      System.out.println("Please write the number in the position"+index+": ");
+      System.out.println("Please write the number in the vectorA: ");
       vecA[index] = sc.nextInt();
-      
+      System.out.println("Please write the number in the vectorB: ");
+      vecB[index] = sc.nextInt();
+      System.out.println("Please write the number in the vectorC: ");
+      vecC[index] = sc.nextInt();
 
+      readVector(vecA,vecB,vecC,sc,index+1);
     }
   }
+
+  public static Long[] modularCalculator(Long base[],Long exp[],Long module[],Integer index)
+  {
+
+  }
+
 
   public static Long calculateModular(Long base, Long exp, Long module) {
     if (module == 1) {

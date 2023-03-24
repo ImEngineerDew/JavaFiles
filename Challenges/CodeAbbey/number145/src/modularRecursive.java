6bf1a369d8ab0a4e846914655878790da85062e3
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-03-24 15:20:31 -0500

    Writing the code lines for data inputs

--- Challenges/CodeAbbey/number145/src/modularRecursive.java
+++ Challenges/CodeAbbey/number145/src/modularRecursive.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 public class modularRecursive {
-  public static void main(String args[]) {
-
+  public static void main(String args[])
+  {
+    //Long res = calculateModular(14L,28L,219431273L);
+    System.out.println(res);
   }
 
-  public Long calculateModular(Long base, Long exp, Long module) {
+  public static void readVector(Integer vecA[],Integer vecB[],Integer vecC[],Scanner sc, Integer index)
+  {
+    if(!index.equals(vecA.length))
+    {
+      System.out.println("Please write the number in the position"+index+": ");
+      vecA[index] = sc.nextInt();
+      
+
+    }
+  }
+
+  public static Long calculateModular(Long base, Long exp, Long module) {
     if (module == 1) {
       return 0L;
     }
@@ -16,6 +30,7 @@ public class modularRecursive {
     if (exp % 2 == 1) {
       result = (result * base) % module;
     }
+    System.out.println(temporal);
     return result;
   }
 }

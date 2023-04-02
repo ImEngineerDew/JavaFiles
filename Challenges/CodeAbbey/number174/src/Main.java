383f3a92fb05d158532d64b19dcf352dda1d57a6
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-04-01 23:30:30 -0500

    I found the light of end

--- Challenges/CodeAbbey/number174/src/Main.java
+++ Challenges/CodeAbbey/number174/src/Main.java
@@ -1,21 +1,28 @@
+import java.math.*;
+
 public class Main {
   public static void main(String args[]) {
     int kValue = 63;
     int nValue = 33;
 
-    double radio = (long) Math.pow(10, kValue);
-    double diameter = radio;
+    BigInteger radio = BigInteger.TEN.pow(kValue);
+    BigInteger diameter = radio;
 
-    for (int i = 0; i < nValue; i++) {
-      double middlePoint = diameter / 2L;
-      long newIssue = (long) (Math.pow(radio, 2) - Math.pow(middlePoint, 2));
+    for (Integer i = 1; i <=nValue; i++) {
+      BigInteger middlePoint = diameter.divide(BigInteger.valueOf(2));
+      BigInteger newIssue = radio.pow(2).subtract(middlePoint.pow(2));
 
-      double root = Math.sqrt(newIssue);
-      double powMiddle = Math.pow(middlePoint, 2);
-      diameter = Math.sqrt(radio + Math.pow((radio - root), 2));
+      BigDecimal root = new BigDecimal(Math.sqrt(newIssue.doubleValue()));
+      BigDecimal powMiddle = new BigDecimal(Math.pow(middlePoint.doubleValue(),2));
 
 
-      System.out.println("Iteration: " + (i + 1) + " Value of pi: " +);
+
+      /**
+      diameter = Math.sqrt(powMiddle + (Math.pow((radio-root),2)));
+
+      double pi = diameter*6*Math.pow(2,i/2);
+      */
+      System.out.println(i+" "+root+" "+powMiddle);
     }
   }
 }

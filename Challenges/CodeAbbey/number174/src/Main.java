18710e1a4a11a5ffb627e7faaa4bd5da56a51593
ImEngineerDew <dewin.acosta9207@gmail.com>
2023-04-01 19:42:20 -0500

    Almost is ready

--- Challenges/CodeAbbey/number174/src/Main.java
+++ Challenges/CodeAbbey/number174/src/Main.java
@@ -1,7 +1,21 @@
-public class Main
-{
-  public static void main(String args[])
-  {
+public class Main {
+  public static void main(String args[]) {
+    int kValue = 63;
+    int nValue = 33;
 
+    double radio = (long) Math.pow(10, kValue);
+    double diameter = radio;
+
+    for (int i = 0; i < nValue; i++) {
+      double middlePoint = diameter / 2L;
+      long newIssue = (long) (Math.pow(radio, 2) - Math.pow(middlePoint, 2));
+
+      double root = Math.sqrt(newIssue);
+      double powMiddle = Math.pow(middlePoint, 2);
+      diameter = Math.sqrt(radio + Math.pow((radio - root), 2));
+
+
+      System.out.println("Iteration: " + (i + 1) + " Value of pi: " +);
+    }
   }
 }

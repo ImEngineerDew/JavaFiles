dbbc6ab32d186ebe618a3ae9ff83dbdf73a3a660
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-04 11:43:56 -0500

    Refurbishing the code again

--- Challenges/CodeAbbey/number174/src/piRecursive.java
+++ Challenges/CodeAbbey/number174/src/piRecursive.java
@@ -1,36 +1,24 @@
 import java.util.Scanner;
 import java.math.BigInteger;
 import java.math.BigDecimal;
-public class piRecursive{
-  public static void main(String args[])
-  {
-      Scanner object = new Scanner(System.in);
 
-      int kValue = object.nextInt();
-      int nValue = object.nextInt();
+public class piRecursive {
+    public static void main(String args[]) {
+        Scanner object = new Scanner(System.in);
 
-      int index = 0;
+        int kValue = object.nextInt();
+        int nValue = object.nextInt();
 
-      /** That issue can print the pi **/
-      recursivePi(kValue, nValue,index+1);
-  }
-  public static void recursivePi(int kValue, int nValue, int index)
-  {
-      /** Lets's check that the diameter value is the same of radio taken before **/
-      BigInteger radio = BigInteger.valueOf(10).pow(kValue);
+        BigInteger radio = BigInteger.TEN.pow(kValue);
+        BigInteger diameter = radio;
+        BigInteger pi = BigInteger.ZERO;
 
-      /** The conditiona if works properly at this recursion **/
-      if(index<nValue)
-      {
-          BigInteger middle = radio.divide(BigInteger.valueOf(2));
-          BigInteger newIssue = radio.pow(2).subtract(middle.pow(2));
+        /** Space to call the recursive method **/
+    }
+    public static void piRecursive()
+    {
+        
+    }
 
-          BigDecimal sqRoot = new BigDecimal(Math.sqrt(newIssue.doubleValue()));
-          BigDecimal powMiddle =  new BigDecimal(Math.pow(middle.doubleValue(),2));
-
-          /** Debbuging this code **/
-          System.out.println(index+": "+middle+" "+newIssue);
-          recursivePi(kValue,nValue,index+1);
-      }
-  }
 }
+

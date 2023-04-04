f20590c0066ba17123835c0513f99919a8e4aeab
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-04 14:10:31 -0500

    Convert my recursive code on Kotlin

---
+++ Challenges/CodeAbbey/number174/src/kotlinPi.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+import java.math.BigInteger
+import java.math.BigDecimal
+
+fun main()
+{
+    val obj = Scanner(System.`in`)
+    val kVal = obj.nextInt()
+    val nVal = obj.nextInt()
+
+    val radio = BigInteger.TEN.pow(kVal)
+    val diameter = radio
+    val pi = BigInteger.ZERO
+}

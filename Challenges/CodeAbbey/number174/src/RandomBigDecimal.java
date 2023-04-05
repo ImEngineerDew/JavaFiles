cd71ff7aac20c093316b62af2cb886e49d02b982
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-05 10:20:20 -0500

    Approaching to exact value

--- Challenges/CodeAbbey/number174/src/RandomBigDecimal.java
+++ Challenges/CodeAbbey/number174/src/RandomBigDecimal.java
@@ -2,6 +2,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class RandomBigDecimal {
@@ -20,20 +21,21 @@ public class RandomBigDecimal {
         BigDecimal resultBase32 = decimalA.divide(decimalB, new MathContext(32, RoundingMode.HALF_EVEN));
         BigDecimal resultBase64 = decimalA.divide(decimalB,new MathContext(64,RoundingMode.HALF_EVEN));
         BigDecimal resultBase128 = decimalA.divide(decimalB, new MathContext(128, RoundingMode.HALF_EVEN));
+        BigDecimal resultUnlimited = decimalA.divide(decimalB,(MathContext.UNLIMITED));
 
         BigDecimal multiply32 = resultBase32.multiply(BigDecimal.valueOf(1000000000));
         BigDecimal multiply64 = resultBase64.multiply(BigDecimal.valueOf(1000000000));
         BigDecimal multiply128 = resultBase128.multiply(BigDecimal.valueOf(1000000000));
+        BigDecimal multiplyUnlimited = resultUnlimited.multiply(BigDecimal.valueOf(1000000000));
+
         BigInteger base32 = multiply32.toBigInteger();
         BigInteger base64 = multiply64.toBigInteger();
         BigInteger base128 = multiply128.toBigInteger();
+        BigInteger unlimited = multiplyUnlimited.toBigInteger();
 
-        //System.out.println("ceiling: " + resultCeiling);
-        //System.out.println("Half up: " + resultHalf);
-        //System.out.println("round down: " + resultDown);
-        //System.out.println("round floor: " + resultFloor);
         System.out.println("Base 32: " + base32);
         System.out.println("Base 64: "+base64);
         System.out.println("Base 128: " +base128);
+        System.out.println("Unlimited: "+unlimited);
     }
 }

b1461cf6c32f8f84b377d61ae9beaabf029cb494
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-05 09:43:40 -0500

    Still testing with decimals

--- Challenges/CodeAbbey/number174/src/RandomBigDecimal.java
+++ Challenges/CodeAbbey/number174/src/RandomBigDecimal.java
@@ -1,12 +1,11 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.Scanner;
-import java.math.BigInteger;
-import java.math.BigDecimal;
-public class RandomBigDecimal
-{
-    public static void main(String args[])
-    {
+
+public class RandomBigDecimal {
+    public static void main(String args[]) {
         Scanner object = new Scanner(System.in);
         BigInteger valueA = object.nextBigInteger();
         BigInteger valueB = object.nextBigInteger();
@@ -14,18 +13,18 @@ public class RandomBigDecimal
         BigDecimal decimalA = new BigDecimal(valueA);
         BigDecimal decimalB = new BigDecimal(valueB);
 
-        BigDecimal resultCeiling = decimalA.divide(decimalB, new MathContext(20, RoundingMode.CEILING));
-        BigDecimal resultHalf = decimalA.divide(decimalB,new MathContext(20,RoundingMode.HALF_UP));
-        BigDecimal resultDown = decimalA.divide(decimalB, new MathContext(20,RoundingMode.DOWN));
-        BigDecimal resultFloor = decimalA.divide(decimalB,BigDecimal.ROUND_FLOOR);
+        BigDecimal resultCeiling = decimalA.divide(decimalB, new MathContext(30, RoundingMode.CEILING));
+        BigDecimal resultHalf = decimalA.divide(decimalB, new MathContext(30, RoundingMode.HALF_UP));
+        BigDecimal resultDown = decimalA.divide(decimalB, new MathContext(30, RoundingMode.DOWN));
+        BigDecimal resultFloor = decimalA.divide(decimalB, new MathContext(30, RoundingMode.FLOOR));
+        BigDecimal resultBase32 = decimalA.divide(decimalB, new MathContext(32, RoundingMode.HALF_EVEN));
+        BigDecimal resultBase128 = decimalA.divide(decimalB, new MathContext(128, RoundingMode.HALF_EVEN));
 
-        System.out.println("Result with ceiling: "+resultCeiling);
-        System.out.println("Result with half even: "+resultHalf);
-        System.out.println("Result with round down: "+resultDown);
-        System.out.println("Result with round floor: "+resultFloor);
-
-
-
+        System.out.println("ceiling: " + resultCeiling);
+        System.out.println("Half up: " + resultHalf);
+        System.out.println("round down: " + resultDown);
+        System.out.println("round floor: " + resultFloor);
+        System.out.println("Base 32: " + resultBase32);
+        System.out.println("Base 128: " + resultBase128);
     }
-
 }

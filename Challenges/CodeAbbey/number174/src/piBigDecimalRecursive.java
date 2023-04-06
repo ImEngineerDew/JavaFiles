adb3e0f7c01ef56a9655a5e35cf2439294a75189
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-06 09:22:36 -0500

    Format code#

--- Challenges/CodeAbbey/number174/src/piBigDecimalRecursive.java
+++ Challenges/CodeAbbey/number174/src/piBigDecimalRecursive.java
@@ -10,17 +10,17 @@ public class piBigDecimalRecursive {
     BigDecimal radio = BigDecimal.TEN.pow(kValue);
     BigDecimal diameter = radio;
 
-    System.out.println(pi(kValue,nValue,radio,diameter,1));
+    System.out.println(pi(kValue, nValue, radio, diameter, 1));
   }
 
   public static BigDecimal pi(int k, int n, BigDecimal rd, BigDecimal dmt, int i) {
     BigDecimal middle = dmt.divide(BigDecimal.valueOf(2));
     BigDecimal newIssue = rd.pow(2).subtract(middle.pow(2));
     BigDecimal root = new BigDecimal(Math.sqrt(newIssue.doubleValue()));
-
-    System.out.println("Middle value: "+middle);
-    System.out.println("newIssue: "+newIssue);
-    System.out.println("Square root of newIssue: "+root);
+    
+    System.out.println("Middle value: " + middle);
+    System.out.println("newIssue: " + newIssue);
+    System.out.println("Square root of newIssue: " + root);
     return BigDecimal.valueOf(0);
 
   }

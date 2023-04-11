41f085bc10f2b5ef7cae2393687fd206b530bfe1
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-11 15:07:02 -0500

    Convert integer to Binary

--- Challenges/CodeAbbey/number179/src/Main.java
+++ Challenges/CodeAbbey/number179/src/Main.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner object = new Scanner(System.in);
-        Integer binary = object.nextInt();
 
-        if(binary>0)
-        {
-            //put your code here!
-        }
+    public static String integerToBinary (int value)
+    {
+        return Integer.toBinaryString(value);
+    }
+    public static void main(String[] args) {
+
+        Scanner object = new Scanner(System.in);
+        int intToBin = object.nextInt();
+
+        String answer = integerToBinary(intToBin);
+        System.out.println(intToBin+" is equals to: "+answer);
     }
 }

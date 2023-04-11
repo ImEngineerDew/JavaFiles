5756e17b3984313c45f90ca4847f389498acbc23
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-11 17:54:38 -0500

    It's alread now

--- Challenges/CodeAbbey/number179/src/Main.java
+++ Challenges/CodeAbbey/number179/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static String integerToBinary(int value) {
         return Integer.toBinaryString(value);
     }
+
     public static void main(String[] args) {
 
         Scanner object = new Scanner(System.in);
@@ -13,8 +14,20 @@ public class Main {
 
         StringBuilder stringBuilder = new StringBuilder();
         int counter = 1;
+        char currentChar = answer.charAt(0);
 
+        for (int i = 1; i < answer.length(); i++) {
+            char nextChar = answer.charAt(i);
+            if (currentChar == nextChar) {
+                counter++;
+            } else {
+                stringBuilder.append(Integer.toBinaryString(counter));
+                currentChar = nextChar;
+                counter = 1;
+            }
+        }
         System.out.println(intToBin + " is equals to: " + answer);
-        System.out.println("Length of this binary: " + answer.length());
+        stringBuilder.append(Integer.toBinaryString(counter));
+        System.out.println("Binary counted: "+stringBuilder);
     }
 }

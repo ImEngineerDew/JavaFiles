0cb3c703ece32f109f1fc2b2eb82bc02f33811ac
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-11 17:41:54 -0500

    Let's work, again!

--- Challenges/CodeAbbey/number179/src/Main.java
+++ Challenges/CodeAbbey/number179/src/Main.java
@@ -5,25 +5,16 @@ public class Main {
     public static String integerToBinary(int value) {
         return Integer.toBinaryString(value);
     }
-
     public static void main(String[] args) {
 
         Scanner object = new Scanner(System.in);
         int intToBin = object.nextInt();
-
-        int zerosCount = 0;
-        int onesCount = 0;
-
         String answer = integerToBinary(intToBin);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int counter = 1;
+
         System.out.println(intToBin + " is equals to: " + answer);
         System.out.println("Length of this binary: " + answer.length());
-
-        /** This code line can count the ones and zeros of whole binary number **/
-        for (int i = 0; i < answer.length(); i++) {
-            if (answer.charAt(i) == '1') {
-                onesCount++;
-            }
-        }
-        System.out.println("Ones count: "+onesCount);
     }
 }

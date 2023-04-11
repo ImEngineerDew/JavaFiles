f1c93cd538e357e7beea65e1d97c6fea4aeef28d
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-11 15:34:37 -0500

    Let's continue

--- Challenges/CodeAbbey/number179/src/Main.java
+++ Challenges/CodeAbbey/number179/src/Main.java
@@ -10,8 +10,16 @@ public class Main {
         Scanner object = new Scanner(System.in);
         int intToBin = object.nextInt();
 
+        int stringLongitude = 1;
+
         String answer = integerToBinary(intToBin);
         System.out.println(intToBin+" is equals to: "+answer);
         System.out.println("Length of this binary: "+answer.length());
+
+        for(int i=0; i<answer.length();i+=stringLongitude)
+        {
+            String segment = answer.substring(i,i+stringLongitude);
+            System.out.println("Binary splited #"+i+":"+segment);
+        }
     }
 }

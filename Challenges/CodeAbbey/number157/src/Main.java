6289889360685f8efc6d0278b365232434f88da8
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-12 14:29:05 -0500

    Now, Let's try

--- Challenges/CodeAbbey/number157/src/Main.java
+++ Challenges/CodeAbbey/number157/src/Main.java
@@ -2,25 +2,37 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)
-    {
-        Scanner object = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner object = new Scanner(System.in);
 
-        System.out.println("Please write the size of your array: ");
-        Integer size = object.nextInt();
+    System.out.println("Please write the size of your array: ");
+    Integer size = object.nextInt();
 
-        BigInteger isPrime[] = new BigInteger[size];
+    System.out.println("Please write the follow numbers: ");
+    BigInteger primes = object.nextBigInteger();
 
+    boolean isPrime[] = new boolean[size];
+
+    for (int i = 0; i < isPrime.length; i++) {
+      isPrime[i] = isPrime(primes);
     }
-    public static boolean isPrime(BigInteger num)
-    {
-        if(num==BigInteger.ZERO||num==BigInteger.ONE||num==BigInteger.valueOf(4))
-        {
-            return false;
-        }
-        for(int i=2; i<num/2;i++)
-        {
+    System.out.println(isPrime.toString());
+  }
 
-        }
+  public static boolean isPrime(BigInteger num) {
+    int numFor = num.intValue();
+    if (num == BigInteger.ZERO || num == BigInteger.ONE || num == BigInteger.valueOf(4)) {
+      return false;
     }
+    for (int i = 2; i < numFor / 2; i++) {
+      if (numFor % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static int eMirpNum(BigInteger number) {
+    return 0;
+  }
 }

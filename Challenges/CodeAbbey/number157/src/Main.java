fc62d64f8fdb89130a36233dac632a9d09d6821c
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-12 14:12:22 -0500

    New tracking with Java files

---
+++ Challenges/CodeAbbey/number157/src/Main.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner object = new Scanner(System.in);
+
+        System.out.println("Please write the size of your array: ");
+        Integer size = object.nextInt();
+
+        BigInteger isPrime[] = new BigInteger[size];
+
+
+    }
+}

503de6fdadcd16004a46eeeb5de74f8927e9cae6
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-13 10:52:58 -0500

    Completed the recursion between isPrime, read and show their vectors

--- Challenges/CodeAbbey/number157/src/testingMain.java
+++ Challenges/CodeAbbey/number157/src/testingMain.java
@@ -11,25 +11,44 @@ public class testingMain {
     BigInteger input[] = new BigInteger[size];
     boolean numberPrime[] = new boolean[size];
     readNumbers(input, numberPrime, object, 0);
-    showNumbers(input,numberPrime,0);
+    showNumbers(input, numberPrime, 0);
   }
 
   public static void readNumbers(BigInteger input[], boolean pri[], Scanner sc, Integer i) {
     if (!i.equals(input.length)) {
       BigInteger primes = sc.nextBigInteger();
       input[i] = primes;
-      pri[i] = Main.isPrime(primes);
+      pri[i] = isPrime(primes, i);
       readNumbers(input, pri, sc, i + 1);
     }
   }
 
   public static void showNumbers(BigInteger input[], boolean pri[], Integer j) {
-    if (j< input.length) {
+    if (j < input.length) {
       BigInteger primes = input[j];
       if (pri[j] == true) {
         System.out.println(primes + " is a prime number");
       }
-      showNumbers(input, pri, j+1);
+      /** I guess that this space must to put here the method
+       * that catches the non-prime number and backwards itself to
+       * calculate their prime number
+       */
+      showNumbers(input, pri, j + 1);
     }
+  }
+
+  public static boolean isPrime(BigInteger num, Integer index) {
+    Integer numFor = num.intValue();
+
+    if (num.equals(BigInteger.ZERO) || num.equals(BigInteger.ONE) || numFor.equals(4)) {
+      return false;
+    }
+    if (index > (numFor) / 2) {
+      if (numFor % index == 0) {
+        return false;
+      }
+    }
+    isPrime(num, index + 1);
+    return true;
   }
 }

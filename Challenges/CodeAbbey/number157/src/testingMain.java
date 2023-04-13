be48b292a36af3d83cd4ec9b7e6f0b6ffc3c27e0
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-13 09:55:49 -0500

    Minor changes

--- Challenges/CodeAbbey/number157/src/testingMain.java
+++ Challenges/CodeAbbey/number157/src/testingMain.java
@@ -7,5 +7,17 @@ public class testingMain {
 
     System.out.println("Please write the size of your array: ");
     Integer size = object.nextInt();
+
+    BigInteger input[] = new BigInteger[size];
+    boolean numberPrime[] = new boolean[size];
   }
+  public static void readVector(BigInteger input[], boolean pri[], Scanner sc, int i, int j)
+  {
+    if(i== input.length) {
+      BigInteger primes = sc.nextBigInteger();
+      input[i] = primes;
+      pri[i] = Main.isPrime(primes);
+    }
+  }
+
 }

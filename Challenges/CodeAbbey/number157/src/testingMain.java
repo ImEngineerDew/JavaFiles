915825d45b85c2a3e6da395e7767f6b06cf5bb1d
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-13 09:47:45 -0500

    Create a scanner class for size input

--- Challenges/CodeAbbey/number157/src/testingMain.java
+++ Challenges/CodeAbbey/number157/src/testingMain.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 import java.math.BigInteger;
-public class testingMain {
-  public static void main(String args[])
-  {
 
+public class testingMain {
+  public static void main(String args[]) {
+    Scanner object = new Scanner(System.in);
+
+    System.out.println("Please write the size of your array: ");
+    Integer size = object.nextInt();
   }
 }

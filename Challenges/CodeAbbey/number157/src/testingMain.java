f173502fcf68c0ad9c90e53416fe2b4c33920711
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-13 10:40:09 -0500

    Recursion partially completed

--- Challenges/CodeAbbey/number157/src/testingMain.java
+++ Challenges/CodeAbbey/number157/src/testingMain.java
@@ -5,19 +5,33 @@ public class testingMain {
   public static void main(String args[]) {
     Scanner object = new Scanner(System.in);
 
-    System.out.println("Please write the size of your array: ");
+    System.out.print("Please write the size of your array: ");
     Integer size = object.nextInt();
 
     BigInteger input[] = new BigInteger[size];
     boolean numberPrime[] = new boolean[size];
+    readNumbers(input, numberPrime, object, 0);
+    showNumbers(input,numberPrime,0);
   }
-  public static void readVector(BigInteger input[], boolean pri[], Scanner sc, int i, int j)
-  {
-    if(i== input.length) {
+
+  public static void readNumbers(BigInteger input[], boolean pri[], Scanner sc, Integer i) {
+    if (!i.equals(input.length)) {
       BigInteger primes = sc.nextBigInteger();
       input[i] = primes;
       pri[i] = Main.isPrime(primes);
+      readNumbers(input, pri, sc, i + 1);
     }
   }
 
+  public static void showNumbers(BigInteger input[], boolean pri[], Integer j) {
+    if (j< input.length) {
+      BigInteger primes = input[j];
+      if (pri[j] == true) {
+        System.out.println(primes + " is a prime number");
+      } else {
+        System.out.println(primes + " isn't a prime number");
+      }
+      showNumbers(input, pri, j+1);
+    }
+  }
 }

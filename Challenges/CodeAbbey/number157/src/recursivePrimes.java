34492fd3783040f34ed4722e0a61a9cb55b7b9d9
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-14 11:04:47 -0500

    Still works now with the re-implementation2

--- Challenges/CodeAbbey/number157/src/recursivePrimes.java
+++ Challenges/CodeAbbey/number157/src/recursivePrimes.java
@@ -9,6 +9,7 @@ public class recursivePrimes {
     Integer size = object.nextInt();
     BigInteger arrayBigs[] = new BigInteger[size];
     readBigsVector(arrayBigs,object,0);  //readVector starting here
+    showBigsVector(arrayBigs,0);
   }
 
   public static void readBigsVector(BigInteger vec[], Scanner sc, int i) {
@@ -16,6 +17,15 @@ public class recursivePrimes {
       System.out.print("Write your numbers to evaluate: ");
       vec[i] = sc.nextBigInteger();
       readBigsVector(vec, sc, i + 1);
+    }
+  }
+
+  public static void showBigsVector(BigInteger vec[], int i)
+  {
+    if(i<vec.length)
+    {
+      System.out.println(vec[i]);
+      showBigsVector(vec,i+1);
     }
   }
 }

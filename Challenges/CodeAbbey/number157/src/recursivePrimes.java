b49319d61f1f8cdbdf93a34c1cc48d7d3a695cdf
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-14 11:01:25 -0500

    Enable this recursive method

--- Challenges/CodeAbbey/number157/src/recursivePrimes.java
+++ Challenges/CodeAbbey/number157/src/recursivePrimes.java
@@ -5,14 +5,15 @@ public class recursivePrimes {
   public static void main(String args[]) {
     Scanner object = new Scanner(System.in);
     System.out.print("Please write the size of your array: ");
-    Integer size = object.nextInt();
 
+    Integer size = object.nextInt();
     BigInteger arrayBigs[] = new BigInteger[size];
+    readBigsVector(arrayBigs,object,0);  //readVector starting here
   }
 
   public static void readBigsVector(BigInteger vec[], Scanner sc, int i) {
     if (i < vec.length) {
-      System.out.println("Write your numbers to evaluate: ");
+      System.out.print("Write your numbers to evaluate: ");
       vec[i] = sc.nextBigInteger();
       readBigsVector(vec, sc, i + 1);
     }

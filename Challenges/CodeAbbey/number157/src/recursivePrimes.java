d5400b64d6bf00199a9b4b8d17d68c03070b3d83
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-14 10:59:11 -0500

    Create the recurisve methods of read and show vectors (recursive forms of for cycles)

--- Challenges/CodeAbbey/number157/src/recursivePrimes.java
+++ Challenges/CodeAbbey/number157/src/recursivePrimes.java
@@ -1,15 +1,20 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class recursivePrimes
-{
-    public static void main(String args[])
-    {
-        Scanner object = new Scanner(System.in);
-        System.out.print("Please write the size of your array: ");
-        Integer  size = object.nextInt();
+public class recursivePrimes {
+  public static void main(String args[]) {
+    Scanner object = new Scanner(System.in);
+    System.out.print("Please write the size of your array: ");
+    Integer size = object.nextInt();
 
-        BigInteger arrayBigs[] = new BigInteger[size];
+    BigInteger arrayBigs[] = new BigInteger[size];
+  }
 
+  public static void readBigsVector(BigInteger vec[], Scanner sc, int i) {
+    if (i < vec.length) {
+      System.out.println("Write your numbers to evaluate: ");
+      vec[i] = sc.nextBigInteger();
+      readBigsVector(vec, sc, i + 1);
     }
+  }
 }

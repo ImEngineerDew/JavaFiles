f95eef49013e1bc3bf8a3c0dabfb4a2b974b92a5
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-15 09:17:29 -0500

    Implementing an analog isProbablePrime method

---
+++ Challenges/CodeAbbey/number157/src/testingRecursive.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class testingRecursive {
+  public static void main(String args[]) {
+    Scanner object = new Scanner(System.in);
+    System.out.println("Please write the size of your array: ");
+    Integer size = object.nextInt();
+
+    BigInteger arrayBigs[] = new BigInteger[size];
+  }
+
+}

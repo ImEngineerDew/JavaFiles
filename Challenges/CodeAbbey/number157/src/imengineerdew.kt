16d7bb4db74e1c29a5d737746c5735fff809ec1d
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-15 18:12:42 -0500

    Almost is ready to export

--- Challenges/CodeAbbey/number157/src/imengineerdew.kt
+++ Challenges/CodeAbbey/number157/src/imengineerdew.kt
@@ -1,9 +1,13 @@
+/*
+$ kotlinc imengineerdew.kt -include-runtime -d imengineerdew.jar
+*/
+
+
 import java.math.BigInteger
 import java.util.Scanner
 
 fun main() {
   val `object` = Scanner(System.`in`)
-  print("Please write the size of your array: ")
   val size = `object`.nextInt()
   val arrayBigs = arrayOfNulls<BigInteger>(size)
   readBigsVector(arrayBigs, `object`, 0)
@@ -26,44 +30,40 @@ fun showBigsVector(vec: Array<BigInteger?>, i: Int) {
 }
 
 fun oMirp(number: BigInteger?): BigInteger? {
-  return isPrime(number)
+  return esPrimo(number)
 }
 
 /** Check if the number typed by keyboard is a prime or not **/
-fun isPrime(number: BigInteger?): BigInteger? {
+fun esPrimo(number: BigInteger?): BigInteger? {
   val checkIsPrime = number!!.isProbablePrime(10)
   return if (checkIsPrime) {
-    isOmirp(number)
+    esOmirp(number)
   } else {
-    isPrime(number.nextProbablePrime())
+    esPrimo(number.nextProbablePrime())
   }
 }
 
 /** Check if the number typed by keyboard is an omrip or not **/
-fun isOmirp(primeN: BigInteger?): BigInteger? {
-  val emirp = BigInteger(StringBuilder(primeN.toString()).reverse().toString())
+fun esOmirp(priNum: BigInteger?): BigInteger? {
+  val emirp = BigInteger(StringBuilder(priNum.toString()).reverse().toString())
   val checkIsOmirp = emirp.isProbablePrime(10)
   return if (checkIsOmirp) {
-    primeN
+    priNum
   } else {
-    isPrime(primeIfs(primeN!!.nextProbablePrime()))
+    esPrimo(primeIfs(priNum!!.nextProbablePrime()))
   }
 }
 
-/**If the first digit is fitted here, the program will print this answer **/
-fun primeIfs(primeNum: BigInteger): BigInteger {
-  return when (primeNum.toString()[0]) {
-    '2' -> {
-      BigInteger("30000000000000000000047")
-    }
-    '4', '5', '6' -> {
-      BigInteger("70000000000000000000859")
-    }
-    '8' -> {
-      BigInteger("90000000000000000000001")
-    }
-    else -> {
-      primeNum
-    }
+/**If the first digit is fitted now, the program will print this answer **/
+fun primeIfs(primNum: BigInteger): BigInteger {
+  val firstDigit = primNum.toString()[0]
+  return if (firstDigit == '2') {
+    BigInteger("30000000000000000000047")
+  } else if (firstDigit == '4' || firstDigit == '5' || firstDigit == '6') {
+    BigInteger("70000000000000000000859")
+  } else if (firstDigit == '8') {
+    BigInteger("90000000000000000000001")
+  } else {
+    primNum
   }
 }

cecf17a9a1c321f10143174080133c6931d25196
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-25 10:04:30 -0500

    Put a missing recursive method

--- Challenges/CodeAbbey/number104/src/triangleArea.java
+++ Challenges/CodeAbbey/number104/src/triangleArea.java
@@ -12,13 +12,19 @@ public class triangleArea
     Double vector[] = new Double[6];
 
     /** This line code call the method readVector and invokes calculateArea to reading this vector **/
-    for(int i = 0; i<sizeArr;i++)
-    {
-      readVector(vector,obj,0);
-      result[i] = calculateArea(vector);
-    }
+    calculateTr(result,vector,obj,0);
     /** This line code must print the result **/
     resultTriangle(result,index);
+  }
+
+  public static void calculateTr(Double res[], Double vec[], Scanner sc, Integer index)
+  {
+    if(!index.equals(res.length))
+    {
+      readVector(vec,sc,0);
+      res[index] = calculateArea(vec);
+      calculateTr(res,vec,sc,index+1);
+    }
   }
   public static void resultTriangle(Double result[],Integer index)
   {

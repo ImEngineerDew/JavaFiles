a7e115213d1c6e649a212c167c979ec2a8cd3ae5
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-09 08:52:48 -0500

    Start to rewriting code

--- Challenges/CodeAbbey/number157/src/bigIntegersDef.java
+++ Challenges/CodeAbbey/number157/src/bigIntegersDef.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
 import java.math.BigInteger;
-public class bigIntegersDef
-{
-    public static void main (String args[])
-    {
 
+public class bigIntegersDef {
+  public static void main(String args[]) {
+
+  }
+
+  public static void readBigVectors(BigInteger vec[], Scanner sc, Integer i) {
+    if (i < vec.length) {
+      vec[i] = sc.nextBigInteger();
+      readBigVectors(vec, sc, i + 1);
     }
+  }
+
+  public static void showBigsVector(BigInteger vec[], Integer i) {
+    if (i < vec.length) {
+      BigInteger result = oMirp (vec[i]);
+      System.out.println(result);
+      showBigsVector(vec, i+1);
+    }
+  }
 }

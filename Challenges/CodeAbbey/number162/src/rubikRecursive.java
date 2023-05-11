891ded2a893099ff370d9be8513df414004cd418
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-11 10:55:50 -0500

    Refactoring the code prior to export to Ada

--- Challenges/CodeAbbey/number162/src/rubikRecursive.java
+++ Challenges/CodeAbbey/number162/src/rubikRecursive.java
@@ -1,35 +1,37 @@
 import java.util.*;
 
 public class rubikRecursive {
-  private static HashMap<String, List<Integer>>  rubikCube = new HashMap<>();
+  private static HashMap<String, List<Integer>> rubikCube = new HashMap<>();
+
   public static void main(String[] args) {
     Scanner object = new Scanner(System.in);
     System.out.println("Write the size of your array: ");
     int size = object.nextInt();
 
-    rubikCube.put("F", Arrays.asList(10,11,12,13,14,15,16,17,18));
-    rubikCube.put("B", new Integer[] {0,0,0,0,0,0,0,0});
-    rubikCube.put("L", new Integer[] {0,0,0,0,0,0,0,0});
-    rubikCube.put("R", new Integer[] {0,0,0,0,0,0,0,0});
-    rubikCube.put("U", new Integer[] {0,0,0,0,0,0,0,0});
-    rubikCube.put("D", new Integer[] {0,0,0,0,0,0,0,0});
+    System.out.println("Write the quantity of movements of your cube: ");
+    String moveSides = object.nextLine();
 
-    Integer [] vec = new Integer[size];
-    readVector (vec, object, 0);
-    showVector (vec, 0);
+    rubikCube.put("F", Arrays.asList(10, 11, 12, 13, 14, 15, 16, 17, 18));
+    rubikCube.put("R", Arrays.asList(20, 21, 22, 23, 24, 25, 26, 27, 28));
+    rubikCube.put("D", Arrays.asList(30, 31, 32, 33, 34, 35, 36, 37, 38));
+    rubikCube.put("L", Arrays.asList(40, 41, 42, 43, 44, 45, 46, 47, 48));
+    rubikCube.put("U", Arrays.asList(50, 51, 52, 53, 54, 55, 56, 57, 58));
+    rubikCube.put("B", Arrays.asList(60, 61, 62, 63, 64, 65, 66, 67, 68));
+
+    List<String> movingList = Arrays.asList(moveSides.split(" "));
+    Map<String, List<Integer>> cubeResult = rotateMany(movingList, size, 0, rubikCube);
+    System.out.println(String.join(" "), findStickers(cubeResult));
   }
 
-  public static void readVector(Integer vector[], Scanner sc, int index) {
-    if (index < vector.length) {
-      vector[index] = sc.nextInt();
-      readVector(vector, sc, index + 1);
+  /** Check the amount of rotations **/
+  public static Map<String, List<Integer>> rotateMany(List<String> moves, int size, int index, HashMap<String, List<Integer>> rubikCube) {
+    if (index == size) {
+      return rubikCube;
+    } else {
+      HashMap<String, List<Integer>> newCube = rotates(moves.get(index), rubikCube);
+      return rotateMany(moves, size, index + 1, newCube);
     }
   }
 
-  public static void showVector(Integer vector[], int index) {
-    if (index < vector.length) {
-      System.out.print(vector[index]+" ");
-      showVector(vector, index + 1);
-    }
-  }
+
 }

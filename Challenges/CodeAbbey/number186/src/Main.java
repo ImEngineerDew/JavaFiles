999772d4a7ea0f5613a0f8cac10f9b3d945a910b
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-16 15:53:21 -0500

    Still writing now

--- Challenges/CodeAbbey/number186/src/Main.java
+++ Challenges/CodeAbbey/number186/src/Main.java
@@ -2,7 +2,14 @@ public class Main {
   public static void main(String[] args) {
 
   }
-  public static void simples(int quantity, int index){
+
+  public static void simples(int quantity, int index) {
+    float width = 480;  //pixels
+    float height = 360; //pixels
+    Double scrDistant = 0.5;
+    Double scrWidth = 0.4;
+    Double scrHeight = 0.3;
+    Double eY = 0.1;g
 
   }
 }

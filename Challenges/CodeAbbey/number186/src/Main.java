dc700b23c31f00c0eb196c97baae9a3c5a46de4c
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-16 16:03:13 -0500

    Minor changes

--- Challenges/CodeAbbey/number186/src/Main.java
+++ Challenges/CodeAbbey/number186/src/Main.java
@@ -10,16 +10,20 @@ public class Main {
   public static void simples(int quantity, int index) {
     float width = 480;  //pixels
     float height = 360; //pixels
-    Double scrDistant = 0.5;
-    Double scrWidth = 0.4;
-    Double scrHeight = 0.3;
+    Double scrDistant = 0.5; //This is the distance
+    Double scrWidth = 0.4; // This is the width
+    Double scrHeight = 0.3; // This is the height
     Double eY = 0.1;
 
     if (index < quantity) {
       String tokens[] = object.nextLine().split("\\s+");
-      Float valX = Float.parseFloat(tokens[0]);
-      Float valY = Float.parseFloat(tokens[1]);
-      Float valH = Float.parseFloat(tokens[2]);
+      Float valueOfX = Float.parseFloat(tokens[0]);
+      Float valueOfY = Float.parseFloat(tokens[1]);
+      Float valueOfH = Float.parseFloat(tokens[2]);
+
+      Double valueOfK = scrDistant / valueOfX;
+      Double sX = - valueOfY * valueOfK;
+      Double sY = - eY * valueOfK;
     }
 
   }

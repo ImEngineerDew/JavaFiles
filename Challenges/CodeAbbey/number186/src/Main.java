82840e9609d5e9428260d9f47328cc5589d2784e
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-16 15:58:14 -0500

    Adding a token split line

--- Challenges/CodeAbbey/number186/src/Main.java
+++ Challenges/CodeAbbey/number186/src/Main.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
+  private static Scanner object = new Scanner(System.in);
+
   public static void main(String[] args) {
 
   }
@@ -9,7 +13,14 @@ public class Main {
     Double scrDistant = 0.5;
     Double scrWidth = 0.4;
     Double scrHeight = 0.3;
-    Double eY = 0.1;g
+    Double eY = 0.1;
+
+    if (index < quantity) {
+      String tokens[] = object.nextLine().split("\\s+");
+      Float valX = Float.parseFloat(tokens[0]);
+      Float valY = Float.parseFloat(tokens[1]);
+      Float valH = Float.parseFloat(tokens[2]);
+    }
 
   }
 }

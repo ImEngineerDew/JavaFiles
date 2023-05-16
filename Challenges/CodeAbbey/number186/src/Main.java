69b8c457a08e554ce586ea2270c61a41c4719dd4
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-16 17:30:37 -0500

    Problem 186 has been solved and ready to export from Raku

--- Challenges/CodeAbbey/number186/src/Main.java
+++ Challenges/CodeAbbey/number186/src/Main.java
@@ -5,26 +5,37 @@ public class Main {
 
   public static void main(String[] args) {
 
+    int quantity = Integer.parseInt(object.nextLine());
+    simples(quantity, 0);
   }
 
   public static void simples(int quantity, int index) {
     float width = 480;  //pixels
     float height = 360; //pixels
-    Double scrDistant = 0.5; //This is the distance
-    Double scrWidth = 0.4; // This is the width
-    Double scrHeight = 0.3; // This is the height
-    Double eY = 0.1;
+    double scrDistant = 0.5; //This is the distance
+    double scrWidth = 0.4; // This is the width
+    double scrHeight = 0.3; // This is the height
+    double eY = 1.0;
 
     if (index < quantity) {
       String tokens[] = object.nextLine().split("\\s+");
-      Float valueOfX = Float.parseFloat(tokens[0]);
-      Float valueOfY = Float.parseFloat(tokens[1]);
-      Float valueOfH = Float.parseFloat(tokens[2]);
+      float valueOfX = Float.parseFloat(tokens[0]);
+      float valueOfY = Float.parseFloat(tokens[1]);
+      float valueOfH = Float.parseFloat(tokens[2]);
 
-      Double valueOfK = scrDistant / valueOfX;
-      Double sX = - valueOfY * valueOfK;
-      Double sY = - eY * valueOfK;
+      double valueOfK = scrDistant / valueOfX;
+      double sX = -valueOfY * valueOfK;
+      double sY = -eY * valueOfK;
+      double sH = valueOfH * valueOfK;
+
+      double resOfX = (sX + (scrWidth / 2)) * (width / scrWidth);
+      System.out.printf("%d ", Math.round(resOfX));
+      double resOfY = ((scrHeight / 2) - sY) * (height / scrHeight);
+      System.out.printf("%d ", Math.round(resOfY));
+      double resOfH = ((scrHeight / 2) - sY - sH) * (height / scrHeight);
+      System.out.printf("%d ", Math.round(resOfH));
+
+      simples(quantity, index + 1);
     }
-
   }
 }

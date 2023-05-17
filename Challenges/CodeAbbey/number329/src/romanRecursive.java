1105b47437a28c6108cff94649a603906400ca99
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-17 18:33:51 -0500

    First part of recursion

--- Challenges/CodeAbbey/number329/src/romanRecursive.java
+++ Challenges/CodeAbbey/number329/src/romanRecursive.java
@@ -1,9 +1,13 @@
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Scanner;
-public class romanRecursive
-{
-    public static void main (String args[]){
 
-    }
+public class romanRecursive {
+  public static void main(String args[]) {
+    Scanner object = new Scanner(System.in);
+    int opQuantity = object.nextInt();
+    object.nextLine(); // it makes a jump line
+    performOperations(object, opQuantity);
+    object.close();
+  }
 }

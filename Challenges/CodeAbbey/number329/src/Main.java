8fb06ed6d54c7fba496c5aa910bef9d23a90dcf6
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-17 17:20:16 -0500

    Developing the roman arithmetic

--- Challenges/CodeAbbey/number329/src/Main.java
+++ Challenges/CodeAbbey/number329/src/Main.java
@@ -1,10 +1,15 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner object = new Scanner (System.in);
+
+    System.out.println("Please write the quantity of the ops: ");
+    int opQuantity = object.nextInt();
   }
+
   private static Map<Character, Integer> romanMap = new HashMap<>();
 
   /** List the roman numbers **/
@@ -17,5 +22,24 @@ public class Main {
     romanMap.put('D', 500);
     romanMap.put('M', 1000);
   }
+
+  public static Integer romanToDecimal(String romanNumber) {
+    int numberDec = 0;
+    int previousVal = 0;
+
+    for (int i = romanNumber.length() - 1; i > 0; i--) {
+      char current = romanNumber.charAt(i);
+      int currentVal = romanMap.get(current);
+
+      if (currentVal < previousVal) {
+        numberDec -= currentVal;
+      } else {
+        numberDec += currentVal;
+      }
+      previousVal = currentVal;
+    }
+    return numberDec;
+  }
+
 
 }

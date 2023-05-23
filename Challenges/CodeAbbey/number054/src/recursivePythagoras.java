a3eb06bce02273c3e712f3050ddcc3e3ed8d03dc
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-23 15:28:43 -0500

    New line for this file

--- Challenges/CodeAbbey/number054/src/recursivePythagoras.java
+++ Challenges/CodeAbbey/number054/src/recursivePythagoras.java
@@ -1,8 +1,16 @@
-public class recursivePythagoras
-{
-  public static void main (String args[])
-  {
+import java.util.Scanner;
 
+public class recursivePythagoras {
+  public static void main(String args[]) {
+
+  }
+
+  public static void readVector(Long vector[], Scanner sc, Integer index) {
+    if (index.equals(vector.length)) {
+      return;
+    }
+    Long s = sc.nextLong();
+    vector[index] = calculateSquares(s, 2, 1);
   }
 
 }

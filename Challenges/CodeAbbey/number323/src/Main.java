eb35b3b689c6c6c22929ff632a9cd4491a6581e7
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-29 15:54:31 -0500

    Writing the 'isPalindrome' and 'nextPalindrome' methods

--- Challenges/CodeAbbey/number323/src/Main.java
+++ Challenges/CodeAbbey/number323/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static boolean isPalindrome(Integer nValue) {
+        String numberStr = String.valueOf(nValue);
+        String isPal = new StringBuilder(numberStr).reverse().toString();
+        return numberStr.equals(isPal);
+    }
+    public static int nextPalindrome(int number, int nextPal) {
+        if (isPalindrome(nextPal)) {
+            return nextPal - number;
+        } else {
+            nextPalindrome(number, nextPal + 1);
+        }
     }
 }

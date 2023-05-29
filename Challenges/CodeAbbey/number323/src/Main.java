85512688e9896a876a082b802bb0f70d2bc73cdf
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-29 16:32:41 -0500

    Implementing the BigInts on Palindromes

--- Challenges/CodeAbbey/number323/src/Main.java
+++ Challenges/CodeAbbey/number323/src/Main.java
@@ -1,14 +1,38 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
 public class Main {
     public static boolean isPalindrome(Integer nValue) {
         String numberStr = String.valueOf(nValue);
         String isPal = new StringBuilder(numberStr).reverse().toString();
         return numberStr.equals(isPal);
     }
+
     public static int nextPalindrome(int number, int nextPal) {
         if (isPalindrome(nextPal)) {
             return nextPal - number;
         } else {
-            nextPalindrome(number, nextPal + 1);
+            return nextPalindrome(number, nextPal + 1);
         }
+    }
+
+    public static void readVector(BigInteger vector[], Scanner sc, Integer index) {
+        if (index.equals(vector.length)) {
+            return;
+        }
+        vector[index] = sc.nextBigInteger();
+        readVector(vector, sc, index + 1);
+    }
+
+    public static void showVector(BigInteger vector[], Integer index) {
+        if (index.equals(vector.length)) {
+            return;
+        }
+        System.out.println(vector[index]);
+        showVector(vector, index + 1);
+    }
+    public static void main (String args[])
+    {
+
     }
 }

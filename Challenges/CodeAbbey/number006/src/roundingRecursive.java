77996bacd07b1711f0b5a7f617e818c521cfb9a7
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-30 17:19:06 -0500

    Recursion for 'readVector' is set now

--- Challenges/CodeAbbey/number006/src/roundingRecursive.java
+++ Challenges/CodeAbbey/number006/src/roundingRecursive.java
@@ -1,10 +1,16 @@
-public class roundingRecursive
-{
-   public double divisionOperation (int a, int b) {
-       return a/b;
-   }
-   public static void readVector(int vectorA[], int vectorB[],long rnd[], double vectorDiv[])
-   {
+import java.util.Scanner;
 
-   }
+public class roundingRecursive {
+    public double divisionOperation(int a, int b) {
+        return a / b;
+    }
+
+    public static void readVector(int vectorA[], int vectorB[], Scanner sc, Integer index) {
+        if (index.equals(vectorA.length)) {
+            return;
+        }
+        vectorA[index] = sc.nextInt();
+        vectorB[index] = sc.nextInt();
+        readVector(vectorA, vectorB, sc, index + 1);
+    }
 }

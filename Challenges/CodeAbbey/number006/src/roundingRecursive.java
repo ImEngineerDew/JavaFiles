6b85cc70589c54d9ae7613c1b2f7147b47c2d3d6
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-30 17:07:56 -0500

    Create the methods

--- Challenges/CodeAbbey/number006/src/roundingRecursive.java
+++ Challenges/CodeAbbey/number006/src/roundingRecursive.java
@@ -1,4 +1,10 @@
 public class roundingRecursive
 {
+   public double divisionOperation (int a, int b) {
+       return a/b;
+   }
+   public static void readVector(int vectorA[], int vectorB[],long rnd[], double vectorDiv[])
+   {
 
+   }
 }

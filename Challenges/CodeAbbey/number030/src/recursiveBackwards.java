8a67dac7eb91417c977344a4bcc9ffe844e17641
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-30 23:00:43 -0500

    Problem 030 has been solved now

--- Challenges/CodeAbbey/number030/src/recursiveBackwards.java
+++ Challenges/CodeAbbey/number030/src/recursiveBackwards.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class recursiveBackwards {
+    static Scanner object = new Scanner(System.in);
+    public static String reverseString(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String args[]) {
+        String text = object.nextLine();
+        String backwards = reverseString(text);
+        System.out.println(backwards);
+    }
 }

10eb111ddaffda24a8c8154151b08c9f5d6cc06a
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-05-31 11:25:44 -0500

    Build reverseString and readVector methods

--- Challenges/CodeAbbey/number050/src/Main.java
+++ Challenges/CodeAbbey/number050/src/Main.java
@@ -4,7 +4,15 @@ public class Main {
     static Scanner object = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Please write the amount of you array: ");
+        Integer size = object.nextInt();
+    }
 
+    public static String reverseString(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        return reverseString(str.substring(1)) + str.charAt(0);
     }
 
     public static void readStr(String vecA[], Scanner sc, Integer index) {
